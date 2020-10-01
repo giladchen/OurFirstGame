50c8298e4bc9a7f8403f6743ea2c381b255e8516
guy <guy7182@gmail.com>
2020-10-01 10:42:48 +0300

    added ramp and movement to the player

---
+++ Our First Game/Assets/CameraMov.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMov : MonoBehaviour
+{
+    public Transform playerPos;
+    public Vector3 offset = new Vector3(0,0,-5);
+       // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerPos.transform.position-offset;
+    }
+}
--- Our First Game/Assets/PlayerMovment.cs
+++ Our First Game/Assets/PlayerMovment.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerMovment : MonoBehaviour
 {
+    public ForceMode forceMode=ForceMode.Force;
+    public float dirForce = 1.0f;
+    public Rigidbody playerBody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Game Started");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("w"))
+        {
+            playerBody.AddForce(0, 0, dirForce, forceMode);
+        }
+        if (Input.GetKey("a"))
+        {
+            playerBody.AddForce(-dirForce, 0, 0, forceMode);
+        }
+        if (Input.GetKey("d"))
+        {
+            playerBody.AddForce(dirForce, 0, 0, forceMode);
+        }
+        if (Input.GetKey("s"))
+        {
+            playerBody.AddForce(0, 0, -dirForce, forceMode);
+        }
     }
 }
